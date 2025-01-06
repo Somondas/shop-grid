e3c0b04b815bdcf1076bf4ea196c1112b764e0a8
Somondas <somondas067@gmail.com>
2025-01-06 22:19:51 +0530

    Working on Home Page

---
+++ app/index.jsx
@@ -0,0 +1,67 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+export default function Index() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <View style={styles.imgContainer}>
+          <Image
+            source={require("../assets/images/extra-small-logo.png")}
+            alt="logo"
+            style={{ width: 120, height: 40, resizeMode: "contain" }}
+          />
+        </View>
+        {/* Search Bar */}
+        <View>
+          <TextInput
+            placeholder="What do you want to buy today?"
+            style={styles.textInput}
+            // onChangeText={onSearch}
+          />
+          <TouchableOpacity style={styles.searchBtn}>Search</TouchableOpacity>
+        </View>
+        {/* Categories */}
+        <View></View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#01b45e",
+  },
+  imgContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textInput: {
+    backgroundColor: "#fff",
+    borderRadius: 20,
+    placeholderTextColor: "#01b45e",
+    padding: 10,
+    margin: 10,
+  },
+  searchBtn: {
+    position: "absolute",
+    color: "#fff",
+    right: 3,
+    backgroundColor: "#01b45e",
+    borderRadius: 20,
+    paddingHorizontal: 15,
+    paddingVertical: 7,
+    top: 2,
+    fontWeight: "bold",
+    margin: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
