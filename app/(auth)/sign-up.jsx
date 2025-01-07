45c70b95e824e8b941586611d03d1699e499d541
Somondas <somondas067@gmail.com>
2025-01-07 22:49:34 +0530

    Updates

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const SignUp = () => {
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Image
+          style={styles.img}
+          source={require("../../assets/images/shopgrid-white.png")}
+        />
+        <Text>SignUp</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default SignUp;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+  },
+});
